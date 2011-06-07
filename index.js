4745a93bf9b445f49a25775fecf6a41c1b363ab0
Taras Kunch <taras.kunch@gmail.com>
2011-06-07 18:12:19 +0300

    Added queue adapter and rq

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ module.exports = {
     db: require('./lib/db'),
     storage: require('./lib/storage'),
     logger: require('./lib/logger'),
+    queueAdapter: require('./lib/queue').Adapter,
     misc: require('./lib/misc')
 };
